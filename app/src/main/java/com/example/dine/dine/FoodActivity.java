7ad88c9c3856554e76a45f9f98cf31957a3e8fd2
jamesho <8.james.ho@gmail.com>
2018-12-11 10:34:45 -0500

    Can get currentLocation and nearby places

--- app/src/main/java/com/example/dine/dine/FoodActivity.java
+++ app/src/main/java/com/example/dine/dine/FoodActivity.java
@@ -1,17 +1,31 @@
 package com.example.dine.dine;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
@@ -25,6 +39,7 @@ import java.util.Map;
 
 
 public class FoodActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_ACCESS_FINE_LOCATION = 25;
 
     // TODO(1): (Completed) Display Firestore data in recyclerviews
     // TODO(1.1): (Completed) Add click handlers
@@ -33,6 +48,7 @@ public class FoodActivity extends AppCompatActivity {
     // TODO(4, postponed): check for google play services in onCreate and onResume (https://firebase.google.com/docs/cloud-messaging/android/client#sample-play)
     // TODO(5, postponed): Make a class that handles the firebase tokens. Sending to server/when they reset.
     // TODO(6, postponed): Make a way for the client to subscribe to the firebase topic.
+    // FIXME: Make sure to follow the permissions best practices
 
     private String TAG = this.getClass().getName();
     private FirebaseAuth mAuth;
@@ -42,6 +58,8 @@ public class FoodActivity extends AppCompatActivity {
             .document("aqvUJjyokpta9KyBFz9U")
             .collection("all_items");
     private FirestoreRecyclerAdapter mFirestoreAdapter;
+    protected GeoDataClient mGeoDataClient;
+    protected PlaceDetectionClient mPlaceDetectionClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +67,29 @@ public class FoodActivity extends AppCompatActivity {
         setContentView(R.layout.activity_food);
         setUpRecyclerView();
 
+        // Construct a GeoDataClient.
+        mGeoDataClient = Places.getGeoDataClient(this, null);
+
+        // Construct a PlaceDetectionClient.
+        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+
+        // Check for or request for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Should we show an explanation?
+            Log.d(TAG, "onCreate: permission not granted");
+
+            // No explanation needed; request the permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_ACCESS_FINE_LOCATION);
+            Log.d(TAG, "onCreate: permission requested");
+
+        } else {
+            // Permission already granted, get location
+            Log.d(TAG, "onCreate: permission already granted");
+            getLocation();
+        }
         /**
          * Comment out FCM test code
          */
@@ -88,6 +129,52 @@ public class FoodActivity extends AppCompatActivity {
 //                    }
 //                });
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    getLocation();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+        }
+    }
+
+    /**
+     * calls PlacesAPI's getCurrentLocation and returns a list of nearby places. Updates the UI with the more accurate one
+     */
+    private void getLocation() {
+        // Temporary location tag
+        final TextView location_tv = findViewById(R.id.current_location);
+
+        @SuppressLint("MissingPermission") final Task<PlaceLikelihoodBufferResponse> placeResult = mPlaceDetectionClient.getCurrentPlace(null);
+        placeResult.addOnCompleteListener(new OnCompleteListener<PlaceLikelihoodBufferResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<PlaceLikelihoodBufferResponse> task) {
+                PlaceLikelihoodBufferResponse likelyPlaces = task.getResult();
+
+                String name = likelyPlaces.get(0).getPlace().getName().toString();
+                float probability = likelyPlaces.get(0).getLikelihood();
+                //Toast.makeText(getApplicationContext(), "You are currently at " + name + " with a " + probability + " certainty", Toast.LENGTH_LONG).show();
+                location_tv.setText("Place: " + name + ". Likelyhood: " + String.valueOf(probability));
+                for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                    Log.i(TAG, String.format("Place '%s' has likelihood: %g",
+                            placeLikelihood.getPlace().getName(),
+                            placeLikelihood.getLikelihood()));
+                }
+                likelyPlaces.release();
+            }
+        });
     }
 
     /**
